e23c2152d3efa8799af088d6a7157d55331eb399
Joshua Frazer <joshua.frazer.school@gmail.com>
2022-07-14 14:20:37 -0700

    search & horns dropdown now work together

--- src/components/Main.js
+++ src/components/Main.js
@@ -10,19 +10,42 @@ import Col from 'react-bootstrap/Col';
 class Main extends Component{
     constructor(props){
         super(props);
-        this.state = {currentBeasts: this.props.beasts};
+        //both form components need access to each other's results
+        this.state = {currentBeasts: this.props.beasts, searchBeasts: this.props.beasts, hornsBeasts: this.props.beasts};
     }
 
-    filterBeasts(searchTerm){
-        //strip whitespace from input, ignore capitalization in the regex 
+    //needed to combine both searches into a single setState
+    filterBeasts(searchBeasts, hornsBeasts){
+        //intersection between searchBeasts & hornsBeasts:
+        const newBeasts = searchBeasts.filter(b => hornsBeasts.includes(b))
+        this.setState({currentBeasts: newBeasts, searchBeasts: searchBeasts, hornsBeasts: hornsBeasts});
+    }
+
+    filterSearch(searchTerm){
         const regEx = new RegExp(searchTerm.replace(/\s*/g,""),  "i");
         return this.props.beasts.filter(b => regEx.test(b.title.replace(/\s*/g,"")) || regEx.test(b.keyword));
     }
 
+    filterHorns(numberOfHorns){
+        return this.props.beasts.filter(b => {
+            if (numberOfHorns === "any"){
+                return true;
+            }
+            return (b.horns === parseInt(numberOfHorns));
+        });
+       
+    }
+
+    //on search input change, filter on new search input but use prexisting horns list
     handleSearchInput = (e) => {
-        console.log(e.target.value);
-        let newBeasts = this.filterBeasts(e.target.value);
-        this.setState({currentBeasts: newBeasts});
+        const searchBeasts = this.filterSearch(e.target.value);
+        this.filterBeasts(searchBeasts, this.state.hornsBeasts);
+    }
+
+    //on horns dropdown change, filter on new number of horns but use prexisting search bar list
+    handleDropDown = (e) => {
+        const hornsBeasts = this.filterHorns(e.target.value);
+        this.filterBeasts(this.state.searchBeasts, hornsBeasts);
     }
     
     //I could have returned just a ul here, but thought why not leave it open for adding things in the future
@@ -31,7 +54,15 @@ class Main extends Component{
             <Container fluid>
                 <Form style={{display: "flex", alignItems: "center", justifyContent: "center",  marginTop: "0.5rem"}}>
                     <Form.Label style={{fontWeight: "bold", fontSize: "200%"}}>Search: </Form.Label>
-                    <Form.Control type="input" onChange={this.handleSearchInput} style={{width: "70%", marginLeft: "0.5rem", border: "solid blue 1px"}}/>
+                    <Form.Control type="input" onChange={this.handleSearchInput} style={{width: "50%", marginLeft: "0.5rem", border: "solid blue 1px"}}/>
+                    <Form.Label style={{fontWeight: "bold", fontSize: "200%", marginLeft: "0.5rem"}}>Horns: </Form.Label>
+                    <Form.Select onChange={this.handleDropDown} style={{width: "30%", marginLeft: "0.5rem", border: "solid blue 1px"}}>
+                        <option value="any">Any</option>
+                        {/*set contains all the possible number of horns (without duplicating them), map to <option>*/}
+                        {Array.from(new Set(this.props.beasts.map(b => b.horns))).map(h => {
+                            return <option key={h.toString()} value={h}>{h}</option>
+                        })}
+                    </Form.Select>
                 </Form>
                         
                 <Row xs={1} sm={2}md={3} lg={4} xl={5}>
