08a291e8ebb91b5622430ddd25a6909a4318fecc
Joshua Frazer <joshua.frazer.school@gmail.com>
2022-07-13 20:00:47 -0700

    updated modal headings

--- src/components/SelectedBeast.js
+++ src/components/SelectedBeast.js
@@ -7,7 +7,7 @@ class SelectedBeast extends Component {
         return(
             <Modal show={this.props.showing} onHide={this.props.closeModal} size="xl">
                 <Modal.Header closeButton>
-                    <Modal.Title>Modal heading</Modal.Title>
+                    <Modal.Title>{this.props.title}</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
                     <img src={this.props.picURL} alt={this.props.title} style={{width: "100%"}}></img>
